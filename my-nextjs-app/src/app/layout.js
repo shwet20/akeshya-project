e3340263e7886ce83010904307899b11dcab1429
Shweta Gatade <shwetagatade020@gmail.com>
2024-06-06 20:41:16 +0530

    Basic structure with responsive UI

---
+++ my-nextjs-app/src/app/layout.js
@@ -0,0 +1,26 @@
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import "./globals.css";
+import 'remixicon/fonts/remixicon.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'boxicons/css/boxicons.min.css';
+
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Akeshya",
+  description: "Created By Shweta Gatade",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <Head>
+      <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
